dea0f551bf7647c389fc3f1b7aaf8a96c2dd854e
Angus Yip <ayip@e1z1r4p9.42.us.org>
2017-08-10 23:22:29 -0700

    updated check norm with -oR | o: omit not .c files | -R uses norminette -R CheckForbiddenSourceHeader

--- tools/check_norm.sh
+++ tools/check_norm.sh
@@ -1,1 +1,15 @@
-find . -name \*.c | norminette | awk "NR>4" | sed 's/Norme: ..//g' | tr '\n' '~' | rev | cut -c2- | rev | sed -e $'s/~ex/\\\nex/g' | sort | tr '~' '\n' | sed ''/Error/s//`printf "\033[31mError\033[0m"`/''
+while getopts ":oR" opt; do
+case $opt in
+	o)
+		NORMARGS=$NORMARGS"$(find . -name '*.c' | tr '\\\n' ' ') "
+		;;
+	R)
+		NORMARGS=$NORMARGS"-R CheckForbiddenSourceHeader "
+		;;
+	\?)
+		echo "Invalid option: -$OPTARG" >&2
+		;;
+esac
+done
+# echo $NORMARGS
+norminette $NORMARGS | tr '\n' '~' | rev | cut -c2- | rev | sed -e $'s/~Norme: ../\\\n/g' | sed 's/Norme: ..//' | sort | tr '~' '\n' | sed ''/Error/s//`printf "\033[31mError\033[0m"`/'' | sed ''/Warning/s//`printf "\033[33mWarning\033[0m"`/''
