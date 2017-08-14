1f8206144957c3974dc49075a29d3435487425c4
Angus Yip <ayip@e1z1r6p4.42.us.org>
2017-08-14 15:13:23 -0700

    ft_blacklist still testing

--- tools/ft_blacklist/main.js
+++ tools/ft_blacklist/main.js
@@ -2,13 +2,17 @@
 
 blacklist = ["nmajedaa"];
 
-if (location.url.includes("profile.intra.42.fr")){
+if (location.href.includes("profile.intra.42.fr")){
 	for (var i = 0; i < blacklist.length; i++){
 		uid = document.querySelector(`[data-user-link="${blacklist[i]}"]`);
 		if (uid){
 			remove = uid.parentElement.parentElement.querySelector('.iconf-delete-2-1');
 			if (remove){
-				remove.click()
+				remove.click();
+				setTimeout(function(){
+					alert('ft_blacklist: your correction appointment with "${blacklist[i]}" will be canceled in 15 seconds. Close the profile webpage to cancel.');
+					// document.querySelector('[data-method="delete"]').click()
+				}, 1000 * 15)
 			}
 		}
 	}
