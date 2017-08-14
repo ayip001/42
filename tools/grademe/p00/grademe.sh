001b5e2ccc7e26af1db8e308fd7a1775504318b0
Angus Yip <ayip@e1z1r6p4.42.us.org>
2017-08-14 03:04:40 -0700

    p00 grademe just checks till 20 now

--- tools/grademe/p00/grademe.sh
+++ tools/grademe/p00/grademe.sh
@@ -1,6 +1,6 @@
 norminette */* -R CheckForbiddenSourceHeader
 gcc -Wall -Wextra -Werror main.c ex00/sastantua.c
-for i in $(seq 0 30)
+for i in $(seq 0 20)
 do
 	echo $(printf "Checking diff for size %d" $i);
 	./a.out $i > ft;
