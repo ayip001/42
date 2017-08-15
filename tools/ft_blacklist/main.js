5d8c7e2cd6d76bea63b8019f06cfbb20b91cf796
Angus Yip <ayip@e1z1r6p4.42.us.org>
2017-08-14 20:53:55 -0700

    tools/ft_blacklist functionality complete

--- tools/ft_blacklist/main.js
+++ tools/ft_blacklist/main.js
@@ -1,22 +1,20 @@
 // Written by @ncardoza
 
-blacklist = ["nmajedaa"];
+blacklist = [""]; //TODO create settings page for users to enter their own usernames
 
 if (location.href.includes("profile.intra.42.fr")){
 	for (var i = 0; i < blacklist.length; i++){
+		// checks if usernames in the blacklist is present in the correction imminent phase
 		uid = document.querySelector(`[data-user-link="${blacklist[i]}"]`);
 		if (uid){
-			remove = uid.parentElement.parentElement.querySelector('.iconf-delete-2-1');
-			if (remove){
-				remove.click();
-				setTimeout(function(){
-					alert('ft_blacklist: your correction appointment with "${blacklist[i]}" will be canceled in 15 seconds. Close the profile webpage to cancel.');
-					// document.querySelector('[data-method="delete"]').click()
-				}, 1000 * 15)
-			}
+			// clicks red x, then clicks confirm after suppressing confirmation window
+			window.confirm = function(){return true;}
+			id = uid.parentElement.parentElement.getAttribute("data-scale-team");
+			document.querySelector("#destroyScaleTeamModalContainer_"+id).querySelector('.btn-danger').click()
 		}
 	}
 	setTimeout(function(){
+		// reload the script every 5 minutes because usernames do not dynamically update
 		location.reload();
 	}, 1000 * 60 * 5)
 }
--- tools/ft_blacklist/man.md
+++ tools/ft_blacklist/man.md
@@ -1,3 +1,7 @@
 # ft_blacklist  
 This script reloads your ```profile.intra.42.fr``` page every 5 minutes to check if there's an imminent correction within 15 minutes.  
-If there is, checks the username against the blacklist. If username exists in blacklist, clicks the x to cancel the appointment. 
+If there is, checks the username against the blacklist. If username exists in blacklist, clicks the x to cancel the appointment.  
+## TODO
+1. embed code into a chrome extension  
+2. create settings page for user-entered blacklist  
+3. additional features  
