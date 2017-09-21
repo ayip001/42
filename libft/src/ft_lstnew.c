362fa792b25e8fa413b42cd85c07c2a8114f94b2
Angus Yip <ayip@e1z2r3p39.42.us.org>
2017-09-21 12:57:26 -0700

    working ft_lstnew

--- libft/src/ft_lstnew.c
+++ libft/src/ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: ayip <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/21 05:38:02 by ayip              #+#    #+#             */
-/*   Updated: 2017/09/21 12:23:44 by ayip             ###   ########.fr       */
+/*   Updated: 2017/09/21 12:54:52 by ayip             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,14 @@ t_list	*ft_lstnew(void const *content, size_t content_size)
 
 	if (!(tmp = malloc(sizeof(t_list))))
 		return (NULL);
-	tmp->content = (void*)content;
-	tmp->content_size = (content ? content_size : 0);
+	tmp->content = NULL;
+	tmp->content_size = 0;
 	tmp->next = NULL;
+	if (content)
+	{
+		if (!(tmp->content = ft_memalloc(content_size)))
+			return (NULL);
+		tmp->content = ft_memcpy(tmp->content, content, content_size);
+	}
 	return (tmp);
 }
